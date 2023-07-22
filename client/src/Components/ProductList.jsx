12fdaceacf58194ab03df8751aacd460b5055401
priyanka1565 <priyankaingle250@gmail.com>
2023-07-22 17:20:05 +0530

    unessary code removed

--- client/src/Components/ProductList.jsx
+++ client/src/Components/ProductList.jsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 import Data from "../Data/Products";
 import Card from "./Card";
 import { useNavigate } from "react-router-dom";
-import Navbar from "./Navbar";
+
 
 
 export default function ProductList() {
